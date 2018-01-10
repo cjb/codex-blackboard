49a50bfd1e11bd5424f52bffc982d90f8020ab4c
C. Scott Ananian <cscott@cscott.net>
2018-01-10 13:52:25 -0500

    Update codex-blackboard-hubot-scripts to 2.1.1.
    This turns off emoji support (which is in core now) and add a
    "fox say" demo of the emoji support.

--- packages/codex-blackboard-hubot-scripts/package.js
+++ packages/codex-blackboard-hubot-scripts/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'codex-blackboard-hubot-scripts',
   summary: 'Hubot scripts for codex blackboard',
-  version: '2.1.0',
+  version: '2.1.1',
   git: 'https://github.com/cscott/codex-blackboard-hubot-scripts.git'
 });
 
 Npm.depends({
     // it is annoying that Meteor requires an exact hash here :(
-    "codex-blackboard-hubot-scripts": "https://github.com/cscott/codex-blackboard-hubot-scripts/tarball/465763e2766dae3c3b0b1b9a05dca878479bb1ee",
+    "codex-blackboard-hubot-scripts": "https://github.com/cscott/codex-blackboard-hubot-scripts/tarball/423818ba01f3289f6c7de7c42ab860a601328e4e",
     "coffee-script": "1.10.0",
 });
 
