022a85cbe8d4c9b9a30671a111baabc4e32d4467
C. Scott Ananian <cscott@cscott.net>
2018-01-11 22:11:52 -0500

    Update codex-blackboard-hubot-scripts to 2.1.2.
    This just removes the hubot-tell script, which conflicts with the
    `tell me a joke` command and was non-functional anyway (since we
    don't implement the robot enter/exit methods yet).

--- packages/codex-blackboard-hubot-scripts/package.js
+++ packages/codex-blackboard-hubot-scripts/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'codex-blackboard-hubot-scripts',
   summary: 'Hubot scripts for codex blackboard',
-  version: '2.1.1',
+  version: '2.1.2',
   git: 'https://github.com/cscott/codex-blackboard-hubot-scripts.git'
 });
 
 Npm.depends({
     // it is annoying that Meteor requires an exact hash here :(
-    "codex-blackboard-hubot-scripts": "https://github.com/cscott/codex-blackboard-hubot-scripts/tarball/423818ba01f3289f6c7de7c42ab860a601328e4e",
+    "codex-blackboard-hubot-scripts": "https://github.com/cscott/codex-blackboard-hubot-scripts/tarball/f75ead3e1cb1d4047cdbc26b34a052ffb9005400",
     "coffee-script": "1.10.0",
 });
 
